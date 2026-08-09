62dc0c58c5c65ce2d4fb5819fbfdbbf2ead5bb5e
phoenix-hacking <holdenwgor@gmail.com>
2026-08-09 17:57:56 -0600

    Repair exact member select usage sentinel [ci skip]

--- test_regress/t/t_class_member_sel_used.py
+++ test_regress/t/t_class_member_sel_used.py
@@ -15,9 +15,7 @@ test.compile(verilator_flags2=["--binary", "-Wall"])
 
 test.execute()
 
-test.file_grep_count(test.run_log_filename, r'^\*-\* CLASS MEMBER SELECT USAGE PASSED \*-\*
-
-test.passes()
-, 1)
+test.file_grep_count(
+    test.run_log_filename, r'^\*-\* CLASS MEMBER SELECT USAGE PASSED \*-\*$', 1)
 
 test.passes()
