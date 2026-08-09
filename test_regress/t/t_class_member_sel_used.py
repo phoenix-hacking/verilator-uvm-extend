6332e3b15e053b5343ed36c0b378d8a6ca3d9751
phoenix-hacking <holdenwgor@gmail.com>
2026-08-09 17:56:18 -0600

    Require one member sentinel [ci skip]

--- test_regress/t/t_class_member_sel_used.py
+++ test_regress/t/t_class_member_sel_used.py
@@ -15,6 +15,9 @@ test.compile(verilator_flags2=["--binary", "-Wall"])
 
 test.execute()
 
-test.file_grep(test.run_log_filename, r'CLASS MEMBER SELECT USAGE PASSED')
+test.file_grep_count(test.run_log_filename, r'^\*-\* CLASS MEMBER SELECT USAGE PASSED \*-\*
+
+test.passes()
+, 1)
 
 test.passes()
