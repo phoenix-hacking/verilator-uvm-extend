7bf1ad918f80923af5fad9444816c20bac86cef0
phoenix-hacking <holdenwgor@gmail.com>
2026-08-09 17:38:33 -0600

    Add M08 TLM analysis driver [ci skip]

---
+++ test_regress/t/t_uvm_tlm_analysis_fifo.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# DESCRIPTION: Verilator: UVM TLM analysis fanout, hierarchy, and FIFO flow
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of either the GNU Lesser General Public License Version 3
+# or the Perl Artistic License Version 2.0.
+# SPDX-FileCopyrightText: 2026 Wilson Snyder
+# SPDX-License-Identifier: LGPL-3.0-only OR Artistic-2.0
+
+import vltest_bootstrap
+
+test.priority(50)
+test.scenarios('vlt', 'vltmt')
+test.top_filename = 't/t_uvm_tlm_analysis_fifo.v'
+
+if test.have_dev_gcov:
+    test.skip("Test suite intended for full dev coverage without needing this test")
+
+test.compile(v_flags2=[
+    "--binary",
+    test.build_jobs_groups,
+    "--CFLAGS -O0",
+    "-Wall",
+    "+incdir+t/uvm",
+    "t/uvm/uvm_pkg_all_v2020_3_1_nodpi.svh",
+])
+
+test.execute(all_run_flags=['' if test.verbose else '+UVM_NO_RELNOTES'])
+test.file_grep(test.run_log_filename, r'UVM TLM ANALYSIS FIFO PASSED')
+test.passes()
