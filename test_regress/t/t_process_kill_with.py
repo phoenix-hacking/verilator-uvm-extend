e646cf6e6574abef661d1f837ea72ce2ab5a046c
phoenix-hacking <holdenwgor@gmail.com>
2026-09-08 15:51:51 -0700

    Use the supported expression lifting option in callback tests

--- test_regress/t/t_process_kill_with.py
+++ test_regress/t/t_process_kill_with.py
@@ -14,7 +14,7 @@ for lift in ((True, False) if test.vlt or test.vltmt else (True, )):
     for protect in ((False, True) if test.vlt or test.vltmt else (False, )):
         flags = ['--binary'] + (['--protect-ids'] if protect else [])
         if not lift:
-            flags += ['--no-flift-expr']
+            flags += ['-fno-lift-expr']
         test.compile(verilator_flags2=flags, threads=2 if test.vltmt else 1)
         name = ('protected' if protect else 'plain') + ('' if lift else '_no_lift')
         test.execute(logfile=f'{test.obj_dir}/sim_{name}.log')
